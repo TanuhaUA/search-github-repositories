6a3c821b9ca9dda22ec71fb90bc1a2afc64b8f8f
tanuha <tanuha@zeoalliance.com>
2020-01-20 12:29:29 +0200

    add prod build

---
+++ build/precache-manifest.fec8b9bf3785c7997481c54107826f9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "593402f6315a3efd90a5e46f05ace5fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "901d3174191d873e4a84",
+    "url": "/static/css/main.0f3f7244.chunk.css"
+  },
+  {
+    "revision": "19c4e50e0bb075841085",
+    "url": "/static/js/2.5404c4bc.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.5404c4bc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "901d3174191d873e4a84",
+    "url": "/static/js/main.47d85a7e.chunk.js"
+  },
+  {
+    "revision": "4ac59b275c135ff8d650",
+    "url": "/static/js/runtime-main.926a6f90.js"
+  }
+]);
